d91b0df4301f2ca80bbdebd9df5511194e2381a4
Promise Fru <promisefru1@gmail.com>
2021-07-21 23:26:44 +0100

    created tweet limit threading

--- main.py
+++ main.py
@@ -5,11 +5,27 @@ consumer_secret = ""
 access_token = ""
 access_token_secret = ""
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+def main():
+    status = {}
 
-api = tweepy.API(auth)
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
 
-status = api.update_status("Hello world from SWOB")
+    api = tweepy.API(auth)
 
-print(status)
+    tweet = ""
+
+    n = 280
+    result = [''.join(tweet[i:i+n]) for i in range(0,len(tweet),n)]
+
+    if (len(result) > 1):
+        status = api.update_status(result[0])
+        for i in range(len(result)-1):
+            api.update_status('@{}{}'.format(status._json['user']['screen_name'], result[i+1]), '{}'.format(status._json['id']))
+            return
+    else:
+        api.update_status(result[0])
+        return
+    
+if __name__ == "__main__":
+    main()
