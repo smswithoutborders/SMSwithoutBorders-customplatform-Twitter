0f61fba3f8665ccba96c5db6ad7f743fb53eca46
Promise Fru <promisefru1@gmail.com>
2021-07-21 18:36:40 +0100

    sending tweets active

---
+++ main.py
@@ -0,0 +1,15 @@
+import tweepy
+
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+status = api.update_status("Hello world from SWOB")
+
+print(status)
