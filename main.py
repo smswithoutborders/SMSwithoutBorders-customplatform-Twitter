8b0d74353a69a6e1c959ae9e9dd999f1fcf6a1b0
sh3rlock <wisdomnji@gmail.com>
2021-07-22 02:12:49 +0100

    update: structured for deku router

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
+#!/usr/bin/python3 
+
 import tweepy
+import configparser
 
-consumer_key = ""
-consumer_secret = ""
-access_token = ""
-access_token_secret = ""
+config = configparser.ConfigParser()
+config.read(os.path.dirname(__file__), '', 'configs.ini')
 
-def main():
+auth=None
+consumer_key = config['AUTH']['key']
+consumer_secret = config['AUTH']['secret']
+
+def post_tweet(tweet):
+    api = tweepy.API(auth)
+    tweet_limits = 280
+    split_tweets = [''.join(tweet[i:i+tweet_limits]) for i in range(0,len(tweet),tweet_limits)]
+    # print(result)
+
+    if (len(split_tweets) > 1):
+        status = api.update_status(split_tweets[0])
+        for i in range(len(result)-1):
+            api.update_status('@{}{}'.format(status._json['user']['screen_name'], split_tweets[i+1]), '{}'.format(status._json['id']))
+            return
+    else:
+        api.update_status(split_tweets[0])
+        return
+    
+def execute(protocol, body, userDetails):
+    access_token = userDetails['access_token']
+    access_token_secret = userDetails['acces_token_secret']
+
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
 
-    api = tweepy.API(auth)
-
-    tweet = ""
-
-    n = 280
-    result = [''.join(tweet[i:i+n]) for i in range(0,len(tweet),n)]
-
-    if (len(result) > 1):
-        status = api.update_status(result[0])
-        for i in range(len(result)-1):
-            api.update_status('@{}{}'.format(status._json['user']['screen_name'], result[i+1]), '{}'.format(status._json['id']))
-            return
-    else:
-        api.update_status(result[0])
-        return
-    
 if __name__ == "__main__":
-    main()
+    post_tweet("Hello world"*300)
