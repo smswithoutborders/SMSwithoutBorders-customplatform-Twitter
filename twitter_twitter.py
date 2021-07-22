c24fc8c8879b24ee1d4edaea2b78e3224fea94c1
Sherlock Wisdom <wisdomnji@gmail.com>
2021-07-22 10:29:19 +0000

    update: submodules updated

--- twitter_twitter.py
+++ twitter_twitter.py
@@ -5,7 +5,7 @@ import tweepy
 import configparser
 
 config = configparser.ConfigParser()
-config.read(os.path.dirname(__file__), '', 'configs.ini')
+config.read(os.path.join(os.path.dirname(__file__), '', 'configs.ini'))
 
 auth=None
 consumer_key = config['AUTH']['key']
@@ -17,20 +17,33 @@ def post_tweet(tweet):
     split_tweets = [''.join(tweet[i:i+tweet_limits]) for i in range(0,len(tweet),tweet_limits)]
     # print(result)
 
-    if (len(split_tweets) > 1):
-        status = api.update_status(split_tweets[0])
-        for i in range(len(result)-1):
-            api.update_status('{}'.format(result[i+1]), '{}'.format(status._json['id']), True)
-    else:
-        api.update_status(split_tweets[0])
-        return
+    try:
+        if (len(split_tweets) > 1):
+            status = api.update_status(split_tweets[0])
+            for i in range(len(result)-1):
+                api.update_status('{}'.format(result[i+1]), '{}'.format(status._json['id']), True)
+        else:
+            api.update_status(split_tweets[0])
+            return True
+    except Exception as error:
+        raise Exception(error)
+
+    return False
     
 def execute(protocol, body, userDetails):
-    access_token = userDetails['access_token']
-    access_token_secret = userDetails['acces_token_secret']
+    # print(userDetails)
+    access_token = userDetails['token']['accToken']
+    access_token_secret = userDetails['token']['accTokenSecret']
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
+
+    try:
+        post_tweet(body)
+        return True
+    except Exception as error:
+        print(error)
+        return False
 
 if __name__ == "__main__":
     post_tweet("Hello world"*300)
